bb6d4f3c770109ecbb84b1c1223af706e97950cb
Joshua Bell <belljos@oregonstate.edu>
2019-03-12 16:03:28 -0700

    Renamed main directory

--- univ_web_app/public/searchgames.js
+++
@@ -1,6 +0,0 @@
-function searchGames() {
-    //get the first name 
-    var game_search  = document.getElementById('game_search').value
-    //construct the URL and redirect to it
-    window.location = '/games/search/' + encodeURI(game_search)
-}
